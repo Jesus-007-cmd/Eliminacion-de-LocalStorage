3518bbfb9ad6edd698f19ea2d3c2927411133ef3
Jesus-007-cmd <jesusgtz1@hotmail.com>
2023-02-05 16:55:06 -0600

    4to commit, se crea la eliminación de un elemento del Local Store y se comenzara con el boton para eliminar todos los elementos

--- assets/local_storage.js
+++ assets/local_storage.js
@@ -1,13 +1,11 @@
 const elements=document.querySelector('#elementos')
 const nombres= JSON.parse(localStorage.getItem('nombres'))
-
+const buton_delete = document.querySelector('#elimUno')
 document.addEventListener("DOMContentLoaded", function() {
   localStorage.clear()
 let elementos=[];
 for (i=0; i<=3; i++){
-    
     elementos.push("Elemento"+ (i+1));
-   
 }
 
 localStorage.setItem('Elementos', JSON.stringify(elementos));
@@ -21,6 +19,40 @@ for (let x=0; x<elementos.length; x++){
 
 elements.innerHTML = lista
 
+    
+//elements.options[1].value permite elegir el elemento 1 de los elementos en el select llamados options
+//elements.options.length nos dice el total de elementos en el arreglo
+//alert(elements.id) te muestra el identificador de la clase select html
+//elements.remove(elements.value) te permite eliminar el elemento elegido
+//elements.value ´permite ver el elemento actual seleccionado
 //localStorage.removeItem()
 
+buton_delete.addEventListener('click', function(){
+    
+localStorage.removeItem('Elementos')
+//el siguiente ciclo sera para obtener el index del elemento seleccionado:
+let indexx
+let ele=elements.value
+for (i=0; i<elements.options.length; i++){
+    if (elements.options[i].value==elements.value) {
+        indexx=i;
+        break;
+    }
+}
+elements.remove(indexx);
+    let elementos=[];
+for (i=0; i<elements.options.length; i++){
+    elementos.push(elements.options[i].value);
+}
+
+localStorage.setItem('Elementos', JSON.stringify(elementos));
+alert("Se ha eliminado el elemento "+ele)
+//localStorage.setItem('Elementos', )
+    
+    /*   alert(elements.options.selectindex)
+    alert(elements.options.selectindex)
+    alert(elements.options[elements.selectindex].value)
+   */ 
+    })
 })
+
